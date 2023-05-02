c0f881afcc57d386258e0ccca7c83ef1711f2f47
wngml317@naver.com <wngml317@wisenut.co.kr>
2023-05-02 13:49:51 +0900

    대시보드 빌드 및 배포 수정

--- server/server.ts
+++ server/server.ts
@@ -9,18 +9,17 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
 
-// if (process.env.NODE_ENV === "production") {
-//     app.use(express.static(path.join(__dirname, "../client/build")));
-// }
-// app.get("/", (req, res) => {
-//     res.sendFile(path.join(__dirname, "../client/build", "index.html"));
-// });  
-
-
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.use("/api", Router);
+
+if (process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../client/build")));
+}
+app.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../client/build", "index.html"));
+});  
 
 app.listen(PORT, () => {
     console.log(`⚡ Server is running at http://localhost:${PORT}`)
